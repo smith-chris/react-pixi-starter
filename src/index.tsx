f019393f72df8284eb21c1d1f49ff211e3e0c7b8
smith-chris <kowalski.k@outlook.com>
2019-10-01 13:54:01 +0200

    Seems like with polyfill canvas is sent as an url..

--- src/index.tsx
+++ src/index.tsx
@@ -6,6 +6,22 @@ import { render, AppProvider } from '@inlet/react-pixi'
 import { configureStore } from 'store/configureStore'
 import App from './App'
 import { pixiApp } from 'pixiApp'
+import * as PIXI from 'pixi.js'
+window.PIXI = PIXI
+const t = PIXI.resources.BaseImageResource.crossOrigin
+PIXI.resources.BaseImageResource.crossOrigin = function(...params) {
+  console.log('calling crossOrigin with', params)
+  if (typeof params[1] !== 'string') {
+    return
+    if (params[1].toDataURL) {
+      params[1] = params[1].toDataURL()
+      console.log('canvas to', params[1])
+    }
+  }
+  console.log('post', params)
+  const res = t.apply(this, params)
+  return res
+}
 
 const store = configureStore()
 
