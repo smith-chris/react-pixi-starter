f6729bdad5998e64a04a657f7e49124dcf341a4a
smith-chris <kowalski.k@outlook.com>
2019-10-16 16:05:42 +0200

    Use ref for isPlaying.

--- src/components/Bird.tsx
+++ src/components/Bird.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useRef } from 'react'
+import React, { useState, useRef, useEffect } from 'react'
 import { Sprite, useTick } from '@inlet/react-pixi'
 import { Point } from 'pixi.js'
 import { designWidth, designHeight } from 'setup/dimensions'
@@ -13,20 +13,53 @@ const getImage = (v: number) => {
   return mid
 }
 
-export const Bird = ({ x = designWidth / 3, y = designHeight / 2 }) => {
-  const [variation, setVariation] = useState(0)
+const getY = (variation: number) => designHeight / 2 + Math.round(variation * 5)
+
+export const Bird = ({ x = designWidth / 3 }) => {
   const timePassed = useRef(0)
+  const isPlaying2 = useRef(false)
+  const [variation, setVariation] = useState(0)
+  const [velocity, setVelocity] = useState(0)
+  const [y, setY] = useState(designHeight / 2)
+  const [isPlaying, setIsPlaying] = useState(false)
+
+  useEffect(() => {
+    const touchStart = () => {
+      if (!isPlaying2.current) {
+        setIsPlaying(true)
+        isPlaying2.current = true
+        setY(getY(variation))
+      }
+      setVelocity(6)
+    }
+
+    window.addEventListener('pointerdown', touchStart)
+    window.addEventListener('touchstart', touchStart)
+    return () => {
+      window.removeEventListener('pointerdown', touchStart)
+      window.removeEventListener('touchstart', touchStart)
+    }
+  }, [])
 
   useTick((delta = 0) => {
     timePassed.current += delta
-    setVariation(Math.sin(timePassed.current / 7))
+    if (!isPlaying) {
+      setVariation(Math.sin(timePassed.current / 7))
+    } else {
+      if (y > designHeight) {
+        setIsPlaying(false)
+        isPlaying2.current = false
+      }
+      setVelocity(v => v - 0.25)
+      setY(v => v - velocity)
+    }
   })
 
   return (
     <Sprite
       image={getImage(variation)}
       x={x}
-      y={y + Math.round(variation * 5)}
+      y={isPlaying ? y : getY(variation)}
       anchor={new Point(0.5, 0.5)}
     />
   )
