cb392dd2e7459d6df95e34b9a0c460e7a68d4fa2
smith-chris <kowalski.k@outlook.com>
2020-03-15 20:03:01 +0100

    Basic example.

--- src/setup/index.ts
+++ src/setup/index.ts
@@ -1,5 +1,25 @@
-import { Engine } from 'excalibur/engine'
+import { Engine, Actor, Color, CollisionType } from 'excalibur/engine'
 
-console.log('Hello!!!!!', Engine)
-const add = (a: number) => a + 5
-console.log(add('5'))
+const game = new Engine({
+  width: 800,
+  height: 600,
+  canvasElement: document.getElementById('canvas') as HTMLCanvasElement,
+})
+
+// Create an actor with x position of 150px,
+// y position of 40px from the bottom of the screen,
+// width of 200px, height and a height of 20px
+const paddle = new Actor(150, game.drawHeight - 40, 200, 20)
+
+// Let's give it some color with one of the predefined
+// color constants
+paddle.color = Color.Chartreuse
+
+// Make sure the paddle can partipate in collisions, by default excalibur actors do not collide
+// paddle.collisionType = CollisionType.Fixed
+
+// `game.add` is the same as calling
+// `game.currentScene.add`
+game.add(paddle)
+
+game.start()
