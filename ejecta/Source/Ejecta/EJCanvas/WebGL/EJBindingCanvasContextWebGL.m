14b1149cf8115415a9e7a89d9c61f169f7285024
Krzysiek Kowalski <kowalski.k@outlook.com>
2023-01-10 17:47:21 +0100

    Bring latest Ejecta to the project

--- ejecta/Source/Ejecta/EJCanvas/WebGL/EJBindingCanvasContextWebGL.m
+++ ejecta/Source/Ejecta/EJCanvas/WebGL/EJBindingCanvasContextWebGL.m
@@ -408,7 +408,17 @@ EJ_BIND_FUNCTION(clear, ctx, argc, argv) {
 EJ_BIND_FUNCTION_DIRECT(clearColor, glClearColor, red, green, blue, alpha);
 EJ_BIND_FUNCTION_DIRECT(clearDepth, glClearDepthf, depth);
 EJ_BIND_FUNCTION_DIRECT(clearStencil, glClearStencil, s);
-EJ_BIND_FUNCTION_DIRECT(colorMask, glColorMask, red, green, blue, alpha);
+EJ_BIND_FUNCTION(colorMask, ctx, argc, argv) {
+    if (argc != 4) { return NULL; }
+    
+    EJ_UNPACK_ARGV(GLboolean red, GLboolean green, GLboolean blue, GLboolean alpha);
+    
+    if ([renderingContext alphaShouldLock]) {
+        alpha = GL_FALSE;
+    }
+    
+    glColorMask(red, green, blue, alpha);
+}
 
 EJ_BIND_FUNCTION(compileShader, ctx, argc, argv) {
 	if( argc < 1 ) { return NULL; }
@@ -1233,8 +1243,10 @@ EJ_BIND_FUNCTION_DIRECT(hint, glHint, target, mode);
 
 #undef EJ_BIND_IS_OBJECT
 
-
-EJ_BIND_FUNCTION_DIRECT(isEnabled, glIsEnabled, cap);
+EJ_BIND_FUNCTION(isEnabled, ctx, argc, argv) {
+	EJ_UNPACK_ARGV(GLenum cap);
+	return JSValueMakeBoolean(ctx, glIsEnabled(cap));
+}
 
 EJ_BIND_FUNCTION(isTexture, ctx, argc, argv) {
 	if( argc < 1 ) { return NULL; }
